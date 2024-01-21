5249b9883228cab5d695a9ddc3a2652013b8197b
mayankr1307 <mayankr20027@gmail.com>
2024-01-21 18:11:10 +0530

    Commit 5, Added Base Activity.

---
+++ app/src/main/java/android/project/planweave/activities/BaseActivity.kt
@@ -0,0 +1,72 @@
+package android.project.planweave.activities
+
+import android.app.Dialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.project.planweave.R
+import android.project.planweave.databinding.ActivityBaseBinding
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+class BaseActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce = false
+
+    private var binding: ActivityBaseBinding? = null
+    lateinit var progressDialog: Dialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBaseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+
+    fun showProgressDialog() {
+        progressDialog = Dialog(this@BaseActivity)
+        progressDialog.setContentView(R.layout.progress_dialog)
+
+        progressDialog.show()
+    }
+
+    fun hideProgressDialog() {
+        progressDialog.dismiss()
+    }
+
+    fun getCurrentUserId(): String {
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    fun doubleBackToExit() {
+        if(doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(
+            this@BaseActivity,
+            "Click back again to exit",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        Handler().postDelayed({doubleBackToExitPressedOnce = false}, 2500)
+    }
+
+    fun showErrorSnackBar(message: String) {
+        val snackbar = Snackbar.make(findViewById(android.R.id.content),
+            message,
+            Snackbar.LENGTH_LONG)
+
+        val snackbarView = snackbar.view
+        snackbarView.setBackgroundColor(ContextCompat.getColor(this, R.color.snack_bar_error))
+        snackbar.show()
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
+}
