7d7549eb2a5c7fa1d4b3a6317d040e91104636e7
mayankr1307 <mayankr20027@gmail.com>
2024-03-19 16:29:15 +0530

    Commit 20, Added functionality to create a board.

--- app/src/main/java/android/project/planweave/activities/CreateBoardActivity.kt
+++ app/src/main/java/android/project/planweave/activities/CreateBoardActivity.kt
@@ -10,11 +10,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.project.planweave.R
 import android.project.planweave.databinding.ActivityCreateBoardBinding
+import android.project.planweave.firebase.FireStoreClass
+import android.project.planweave.models.Board
 import android.project.planweave.utils.Constants
 import android.provider.MediaStore
+import android.util.Log
+import android.webkit.MimeTypeMap
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import de.hdodenhof.circleimageview.CircleImageView
 import java.io.IOException
 
@@ -27,6 +34,7 @@ class CreateBoardActivity : BaseActivity() {
     private var mBoardImageURL: String = ""
     private var mSelectedImageFileUri: Uri? = null
     private lateinit var mUserName: String
+
 
     companion object {
         private const val PICK_IMAGE_REQUEST_CODE = 2
@@ -51,6 +59,74 @@ class CreateBoardActivity : BaseActivity() {
                 showImageChooser()
             } else {
                 requestPermission()
+            }
+        }
+
+        binding?.btnCreate?.setOnClickListener {
+            if(mSelectedImageFileUri != null) {
+                uploadBoardImage()
+            }else {
+                showProgressDialog("Please wait...")
+                createBoard()
+            }
+        }
+    }
+
+    private fun createBoard() {
+        val assignedUsersArrayList: ArrayList<String> = ArrayList()
+        assignedUsersArrayList.add(getCurrentUserId())
+
+        var board = Board(
+            binding?.etBoardName?.text.toString(),
+            mBoardImageURL,
+            mUserName,
+            assignedUsersArrayList
+        )
+
+        FireStoreClass().createBoard(this@CreateBoardActivity, board)
+    }
+
+    private fun getFileExtension(uri: Uri?): String? {
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri!!))
+
+    }
+
+    private fun uploadBoardImage() {
+        showProgressDialog("Please wait...")
+
+        if(mSelectedImageFileUri != null) {
+            val sRef: StorageReference =
+                FirebaseStorage.getInstance().reference.child("BOARD_IMAGE" +
+                        System.currentTimeMillis()
+                        + "." +
+                        getFileExtension(mSelectedImageFileUri))
+
+            sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener {
+                    taskSnapshot ->
+                Log.i(
+                    "Board Image URL",
+                    taskSnapshot.metadata!!.reference!!.downloadUrl.toString()
+                )
+
+                taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+                        uri ->
+                    Log.i(
+                        "Downloadable Image URL",
+                        uri.toString()
+                    )
+                    mBoardImageURL = uri.toString()
+
+                    createBoard()
+                }
+            }.addOnFailureListener {
+                    exception ->
+                Toast.makeText(
+                    this@CreateBoardActivity,
+                    exception.message,
+                    Toast.LENGTH_LONG
+                ).show()
+
+                hideProgressDialog()
             }
         }
     }
